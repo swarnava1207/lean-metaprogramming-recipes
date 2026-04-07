8ca2f7c8a67fba278a5c004abd4036b40feeef87
Anirudh Gupta <146579014+AnirudhG07@users.noreply.github.com>
2026-04-07 18:26:07 +0530

    fix

--- Cookbook/FileSystem.lean
+++ Cookbook/FileSystem.lean
@@ -25,7 +25,7 @@ number := false
 This chapter covers most FileSystem operations, such as reading and writing files, creating directories, listing directory contents, etc. We mainly use the {name}`System.FilePath` type and the `IO.FS` module from the Lean standard library.
 This chapter adds recipes for other important file formats which are not Datastructures, such as JSONL, which are commonly used. 
 
-It will be useful to check out {ref "data-structures"}[Data Structures} for File Formats which are Data Structures like JSON, TOML, etc. 
+It will be useful to check out {ref "data-structures"}[Data Structures] for File Formats which are Data Structures like JSON, TOML, etc. 
 This chapter uses various {lean}`IO` recipes which you can checkout in {ref "io"}[I/O] chapter beforehand.
 
 {include 1 Cookbook.FileSystem.ReadingFromFile}
